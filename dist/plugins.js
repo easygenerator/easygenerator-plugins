da2bae01b2275ccaf574934803f1cef0bcd3fb20
jaw4ik <urec.savchuk@gmail.com>
2020-12-31 00:26:30 +0200

    chore: wip

--- dist/plugins.js
+++ dist/plugins.js
@@ -11487,7 +11487,7 @@
 	        init();
 
 	        function getElementContent(element) {
-						  return element.innerHTML || element.getAttribute('data-text');
+	            return element.innerHTML || element.getAttribute('data-text');
 	        }
 
 	        function init() {
