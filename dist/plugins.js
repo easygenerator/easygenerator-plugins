9537f828f18b276588c49f485afbf7d24c3682e6
d.malinovskiy <d.malinovskiy@gmail.com>
2021-01-04 09:07:02 +0200

    Revert "fix: revert for review (#35)"
    This reverts commit 8a9fc8c2

--- dist/plugins.js
+++ dist/plugins.js
@@ -8357,17 +8357,30 @@
 
 	    _createClass(ReviewService, [{
 	        key: 'init',
-	        value: function init(reviewApiUrl, courseId) {
+	        value: function init(reviewApiUrl, courseId, authoringToolDomain) {
 	            this.reviewApiUrl = reviewApiUrl;
 	            this.courseId = courseId;
+	            this.authoringToolDomain = authoringToolDomain;
 	        }
 	    }, {
 	        key: 'postComment',
 	        value: function postComment(message, username, useremail, context) {
+	            var data = JSON.stringify({
+	                courseId: this.courseId,
+	                text: message.trim(),
+	                createdByName: username.trim(),
+	                createdBy: useremail.trim(),
+	                context: context
+	            });
+
 	            return $.ajax({
-	                url: this.getApiUrl('api/comment/create'),
-	                data: { courseId: this.courseId, text: message.trim(), createdByName: username.trim(), createdBy: useremail.trim(), context: context ? JSON.stringify(context) : context },
-	                type: 'POST'
+	                url: this.getApiUrl('comments'),
+	                data: data,
+	                type: 'POST',
+	                headers: {
+	                    'Content-Type': 'application/json',
+	                    'X-Authoring-Tool-Domain': this.authoringToolDomain
+	                }
 	            });
 	        }
 	    }, {
--- src/plugins/review/reviewService.js
+++ src/plugins/review/reviewService.js
@@ -1,16 +1,29 @@
 ﻿class ReviewService {
-    init(reviewApiUrl, courseId){
+    init(reviewApiUrl, courseId, authoringToolDomain){
         this.reviewApiUrl=reviewApiUrl;
         this.courseId=courseId;
+        this.authoringToolDomain=authoringToolDomain;
     }
 
-  postComment(message, username, useremail, context) {
-    return $.ajax({
-      url: this.getApiUrl('api/comment/create'),
-      data: { courseId: this.courseId, text: message.trim(), createdByName: username.trim(), createdBy: useremail.trim(), context: context ? JSON.stringify(context) : context },
-      type: 'POST'
-    });
-  }
+    postComment(message, username, useremail, context) {
+        const data = JSON.stringify({
+          courseId: this.courseId,
+          text: message.trim(),
+          createdByName: username.trim(),
+          createdBy: useremail.trim(),
+          context
+        });
+
+        return $.ajax({
+            url: this.getApiUrl('comments'),
+            data,
+            type: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Authoring-Tool-Domain': this.authoringToolDomain
+            }
+        });
+    }
 
     getApiUrl(apiPath) {
         if (this.reviewApiUrl.indexOf('/', this.reviewApiUrl.length - 1) !== -1) {
